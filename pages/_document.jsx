5d728cb6aff5774f48daf927124677b44d2a2739
KyungHwanKim-devs <kkhdevs@gmail.com>
2023-09-03 18:12:47 +0900

    d'

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,8 +2,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
-      <Head></Head>
+    <Html lang="kr">
+      <Head>
+        <title>레디테크 캠퍼스</title>
+        <meta
+          name="description"
+          content="방사선사 국가고시 강의, 취업을 위한 종합 플랫폼"
+        />
+        <meta property="og:title" content="레디테크 캠퍼스" />
+        <meta
+          property="og:description"
+          content="방사선사 국가고시 강의, 취업을 위한 종합 플랫폼"
+        />
+      </Head>
       <body>
         <Main />
         <div id="modal" />
