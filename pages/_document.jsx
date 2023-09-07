f62538af34ce09c763b916fc190eb89f4eb86207
KIM KYUNGHWAN <104489626+KyungHwanKim-devs@users.noreply.github.com>
2023-09-07 10:15:22 +0900

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,15 +4,15 @@ export default function Document() {
   return (
     <Html lang="kr">
       <Head>
-        <title>레디테크 캠퍼스</title>
+        <title>Radi Tech | 방사선사 국가고시 인강, 레디테크</title>
         <meta
           name="description"
-          content="방사선사 국가고시 강의, 취업을 위한 종합 플랫폼"
+          content="방사선사 국가고시 임상 전문 인강 사이트, 일반촬영, 투시조영, CT, 혈관조영, 치료학, 초음파, MRI, 이론 및 문제풀이 제공"
         />
-        <meta property="og:title" content="레디테크 캠퍼스" />
+        <meta property="og:title" content="Radi Tech | 방사선사 국가고시 인강, 레디테크" />
         <meta
           property="og:description"
-          content="방사선사 국가고시 강의, 취업을 위한 종합 플랫폼"
+          content="방사선사 국가고시 임상 전문 인강 사이트, 일반촬영, 투시조영, CT, 혈관조영, 치료학, 초음파, MRI, 이론 및 문제풀이 제공"
         />
       </Head>
       <body>
