9a5efd0b4557c2ed58e584e5803afc377a66a3ed
KyungHwanKim-devs <kkhdevs@gmail.com>
2023-09-11 23:13:49 +0900

    .

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,14 +4,14 @@ export default function Document() {
   return (
     <Html lang="kr">
       <Head>
-        <title>Radi Tech | 방사선사 국가고시 인강, 레디테크</title>
+        <title>레디테크 | 방사선사 국가고시 인강, Radi Tech</title>
         <meta
           name="description"
           content="방사선사 국가고시 임상 전문 인강 사이트, 일반촬영, 투시조영, CT, 혈관조영, 치료학, 초음파, MRI, 이론 및 문제풀이 제공"
         />
         <meta
           property="og:title"
-          content="Radi Tech | 방사선사 국가고시 인강, 레디테크"
+          content="레디테크 | 방사선사 국가고시 인강, Radi Tech"
         />
         <meta
           property="og:description"
