44140ad4b5fd10f1617855a22895891dbce7748f
KyungHwanKim-devs <kkhdevs@gmail.com>
2023-09-11 23:21:12 +0900

    .

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -17,6 +17,10 @@ export default function Document() {
           property="og:description"
           content="방사선사 국가고시 임상 전문 인강 사이트, 일반촬영, 투시조영, CT, 혈관조영, 치료학, 초음파, MRI, 이론 및 문제풀이 제공"
         />
+        <meta
+          name="naver-site-verification"
+          content="f33d14cb87e47fd5c2ce73c6ea7aa6de45daab6a"
+        />
       </Head>
       <body>
         <Main />
