1c7807f183a0e1bddf390b81602b6cb67657f49a
kkh <kkhdevs@gmail.com>
2023-12-24 08:21:45 +0900

    ad

--- components/header_mobile/index.jsx
+++ components/header_mobile/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const HeaderContainer = styled.div`
   display: none;
@@ -17,7 +18,7 @@ const HeaderContainer = styled.div`
     width: 100%;
     margin: 0 auto;
     padding: 0px;
-    display: block;
+    display: flex;
   }
 `;
 
@@ -27,13 +28,27 @@ const MobileLogo = styled.img`
     padding: 0px 5px;
   }
 `;
+const AuthButton = styled.div`
+  margin-right: 20px;
+  font-size: 15px;
+  font-weight: 600;
+  cursor: pointer;
+  width: 15%;
+`;
 
 export default function HeaderMobile() {
+  const router = useRouter();
   return (
     <HeaderContainer>
       <Link href={"/"}>
         <MobileLogo src="/titlelogo.png" alt="레디테크 캠퍼스" />
       </Link>
+      <AuthButton
+        // selected={router.pathname === "/mypage"}
+        onClick={() => router.push("/event")}
+      >
+        이벤트
+      </AuthButton>
     </HeaderContainer>
   );
 }
