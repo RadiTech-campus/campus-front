b3b88108a1048b2e1127ad16d60816fee8225723
kkh <kkhdevs@gmail.com>
2023-12-24 08:57:02 +0900

    ad

--- pages/event/index.tsx
+++ pages/event/index.tsx
@@ -15,136 +15,42 @@ const LectureListContainer = styled.div`
 `;
 
 const TitleContainer = styled.div`
+  background-color: #f59f29;
   display: flex;
   flex-direction: column;
-  padding: 10px 20px 0px;
-  margin-top: 20px;
-  @media (max-width: 650px) {
-    padding: 5px 15px;
-    margin: 0;
-  }
+  align-items: center;
+  /* width: 80%; */
 `;
 
-const MainTitle = styled.div`
-  font-size: 24px;
-  font-weight: bold;
-  margin-top: 10px;
-  @media (max-width: 650px) {
-    font-size: 4vw;
-    color: #0b0d0f;
-    font-weight: 700;
-    letter-spacing: -3%;
-    margin-top: 0px;
-  }
-`;
-const Tags = styled.div`
-  @media (max-width: 650px) {
-    padding: 0;
-    margin: 10px 0px;
-    display: flex;
-    justify-content: flex-start;
-    color: #a2a2a2;
-    font-size: 3.5vw;
-  }
+const ButtonContainer = styled.div`
+  background-color: white;
+  width: 80%;
   display: flex;
-  font-size: 24px;
-  margin-top: 10px;
-  margin-left: 10px;
-  margin-bottom: 20px;
-  color: #a2a2a2;
+  justify-content: center;
+  margin-bottom: 60px;
+  padding-bottom: 60px;
+  margin-top: -10px;
 `;
 
-const Tag = styled.div`
-  @media (max-width: 650px) {
-    border: 1px solid #a2a2a2;
-    border-radius: 60px;
-    margin-right: 10px;
-    padding: 3px 6px;
-    font-weight: 400;
-  }
-  border: 1px solid #a2a2a2;
-  border-radius: 60px;
-  margin-right: 10px;
-  padding: 5px 12px;
-  font-weight: 400;
+const SubButton1 = styled.button`
+  background-color: #6375ff;
+  width: 60%;
+  border-radius: 15px;
+  height: 120px;
+  border: none;
+  color: white;
+  font-size: 50px;
+  font-weight: 700;
 `;
-
-const ClassCardsContainer = styled.div`
-  @media (max-width: 650px) {
-    display: flex;
-    /* flex-wrap: nowrap;
-    overflow: scroll;
-    ::-webkit-scrollbar {
-      display: none;
-    } */
-    padding: 0px 5px;
-  }
-  padding: 0px 0px 20px;
-  display: flex;
-  flex-wrap: wrap;
-`;
-const ClassCard = styled.div`
-  margin: 10px;
-  width: 31%;
-  text-decoration: none;
-  border-radius: 10px;
-  box-shadow: 0 0 10px 0 rgb(0 0 0 / 20%);
-  border-radius: 20px;
-  @media (max-width: 650px) {
-    box-shadow: 0 0 10px 0 rgb(0 0 0 / 20%);
-    border-radius: 20px;
-    width: 45%;
-    margin: 10px auto;
-  }
-  a {
-    @media (max-width: 650px) {
-    }
-    text-decoration: none;
-    border-radius: 10px;
-  }
-`;
-const ClassImage = styled.div`
-  @media (max-width: 650px) {
-  }
-  height: 180px;
-  position: relative;
-  > img {
-    @media (max-width: 650px) {
-      border-radius: 20px 20px 0 0;
-      width: 100%;
-    }
-    border-radius: 10px;
-  }
-`;
-const ClassTitle = styled.div`
-  @media (max-width: 650px) {
-    font-size: 3.2vw;
-    font-weight: 700;
-    padding: 0 0 0 10px;
-    margin: 5px 0 3px 0;
-    /* line-height: 28.96px; */
-    color: #0b0d0f;
-  }
-  font-size: 24px;
-  font-weight: 600;
-  margin: 10px 0px 0px 10px;
-  padding: 10px 5px 0px;
-  color: black;
-`;
-
-const ClassDesc = styled.div`
-  @media (max-width: 650px) {
-    font-size: 3vw;
-    font-weight: 700;
-    padding: 0 0 0 10px;
-    margin: 0 0 10px 0;
-    /* line-height: 26.06px; */
-    color: #818181;
-  }
-  font-size: 20px;
-  padding: 10px 10px 0px;
-  color: #888888c1;
-  margin: 0px 0px 20px 10px;
+const SubButton2 = styled.button`
+  background-color: #83a446;
+  width: 60%;
+  border-radius: 15px;
+  height: 120px;
+  border: none;
+  color: white;
+  font-size: 50px;
+  font-weight: 700;
 `;
 
 export default function Job() {
@@ -155,8 +61,36 @@ export default function Job() {
   return (
     <LectureListContainer>
       {isMobile && (
-        <img src="/long.png" alt="image" style={{ width: "100%" }} />
+        <img
+          src="/long.png"
+          alt="image"
+          style={{ width: "100%", position: "relative" }}
+        />
       )}
+      <TitleContainer>
+        <img src="/event/1.png" alt="image" style={{ width: "100%" }} />
+        <img src="/event/2.png" alt="image" style={{ width: "80%" }} />
+
+        <ButtonContainer>
+          <SubButton1
+            onClick={() => {
+              alert("버튼");
+            }}
+          >
+            구독하기
+          </SubButton1>
+        </ButtonContainer>
+        <img src="/event/3.png" alt="image" style={{ width: "80%" }} />
+        <ButtonContainer>
+          <SubButton2
+            onClick={() => {
+              alert("버튼");
+            }}
+          >
+            구독하기
+          </SubButton2>
+        </ButtonContainer>
+      </TitleContainer>
       {/* <TitleContainer>
         <MainTitle>자소서, 면접, 대학정보 ✍️</MainTitle>
         <Tags>
