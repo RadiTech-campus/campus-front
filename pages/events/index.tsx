2584da8056fbada390c2f2fbc9e796bb1835fdcc
kkh <kkhdevs@gmail.com>
2023-12-24 09:38:06 +0900

    ad

---
+++ pages/events/index.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Link from "next/link";
+import { useIsMobile } from "../../hooks/useIsMobile";
+
+const LectureListContainer = styled.div`
+  @media (max-width: 650px) {
+    width: 100%;
+    margin: 0px auto;
+  }
+  margin: 100px auto 0px;
+  width: 1160px;
+`;
+
+const TitleContainer = styled.div`
+  background-color: #f59f29;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  /* width: 80%; */
+`;
+
+const ButtonContainer1 = styled.div`
+  background-color: white;
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  margin-top: -10px;
+  margin-bottom: 60px;
+  padding-top: 10px;
+  padding-bottom: 20px;
+  > a {
+    background-color: #6375ff;
+    width: 80%;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    font-size: 4.5vw;
+    font-weight: 700;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+  }
+`;
+
+const ButtonContainer2 = styled.div`
+  background-color: white;
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  margin-top: -10px;
+  margin-bottom: 60px;
+  padding-top: 10px;
+  padding-bottom: 20px;
+  > a {
+    background-color: #83a446;
+    width: 80%;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    font-size: 4.5vw;
+    font-weight: 700;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+  }
+`;
+
+export default function Events() {
+  const isMobile = useIsMobile();
+  return (
+    <LectureListContainer>
+      {isMobile && (
+        <img
+          src="/long.png"
+          alt="image"
+          style={{ width: "100%", position: "relative" }}
+        />
+      )}
+      <TitleContainer>
+        <img src="/event/1.png" alt="image" style={{ width: "100%" }} />
+        <img src="/event/2.png" alt="image" style={{ width: "80%" }} />
+
+        <ButtonContainer1>
+          <Link
+            href={{
+              pathname: `/regist`,
+              query: { id: 2 },
+            }}
+          >
+            구독하기
+          </Link>
+        </ButtonContainer1>
+        <img src="/event/3.png" alt="image" style={{ width: "80%" }} />
+        <ButtonContainer2>
+          <Link
+            href={{
+              pathname: `/regist`,
+              query: { id: 3 },
+            }}
+          >
+            구독하기
+          </Link>
+        </ButtonContainer2>
+      </TitleContainer>
+    </LectureListContainer>
+  );
+}
--- pages/regist/index.jsx
+++ pages/regist/index.jsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import { useEffect, useMemo, useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import Modal from "../../components/modal/Modal";
@@ -181,7 +181,8 @@ export default function SignUp() {
     setIsOpen(true);
   };
 
-  const [Selected, setSelected] = useState(4);
+  const [Selected, setSelected] = useState(Number(router.query.id));
+
   const handleSelect = (e) => {
     setSelected(Number(e.target.value));
   };
@@ -202,6 +203,8 @@ export default function SignUp() {
   // 이하 개편
   const { data: productDatas, isLoading: productDatasIsLoading } =
     useGetAllProduct();
+
+  const data = useMemo(() => productDatas || [], [productDatas]);
 
   return (
     <SignUpContainer>
@@ -242,9 +245,9 @@ export default function SignUp() {
               />
             ) : (
               <RegistSelect onChange={handleSelect} value={Selected}>
-                {productDatas &&
-                  productDatas.length > 0 &&
-                  productDatas.map((li, i) => (
+                {data &&
+                  data.length > 0 &&
+                  data.map((li, i) => (
                     <option key={i} value={li.id}>
                       {li.productTitle}
                     </option>
@@ -271,9 +274,9 @@ export default function SignUp() {
               <PriceTitle>가격</PriceTitle>
               <PriceDetail>
                 <PriceContent>
-                  {productDatas &&
-                    productDatas.length > 0 &&
-                    productDatas.find((li) => li.id === Selected).price}
+                  {data &&
+                    data.length > 0 &&
+                    data.find((li) => li.id === Selected).price}
                   원
                 </PriceContent>
               </PriceDetail>
