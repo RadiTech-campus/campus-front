aa145711d3614d17e4cd2bacf5aaea90b5578f32
kkh <kkhdevs@gmail.com>
2024-01-05 20:08:42 +0900

    ad

--- components/header_mobile/index.jsx
+++ components/header_mobile/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "@emotion/styled";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useAuth } from "../../hooks/useAuth";
 
 const HeaderContainer = styled.div`
   display: none;
@@ -28,26 +29,55 @@ const MobileLogo = styled.img`
     padding: 0px 5px;
   }
 `;
-const AuthButton = styled.div`
-  margin-right: 20px;
-  font-size: 15px;
-  font-weight: 600;
-  cursor: pointer;
-  width: 15%;
+const AuthButton = styled.button`
+  @media (max-width: 650px) {
+    margin-right: 15px;
+    font-size: 2.5vw;
+    padding: 3px 6px;
+    font-weight: 600;
+    /* cursor: pointer; */
+    border-radius: 20px;
+    border: none;
+    background-color: black;
+    color: white;
+    width: 25%;
+  }
 `;
 
 export default function HeaderMobile() {
   const router = useRouter();
+  const auth = useAuth();
+  auth.isAuthenticated;
+  console.log("router", router.pathname);
   return (
     <HeaderContainer>
       <Link href={"/"}>
         <MobileLogo src="/titlelogo.png" alt="레디테크 캠퍼스" />
       </Link>
-      {/* <AuthButton
-        onClick={() => router.push("/events")}
-      >
-        이벤트
-      </AuthButton> */}
+      {/* {router.pathname === "/signin" ? (
+        ""
+      ) : (
+        <AuthButton onClick={() => router.push("/signin")}>로그인</AuthButton>
+      )} */}
+      {!auth.isAuthenticated && router.pathname !== "/signin" && (
+        <AuthButton
+          onClick={() => {
+            router.push("/signin");
+          }}
+        >
+          로그인
+        </AuthButton>
+      )}
+      {auth.isAuthenticated && (
+        <AuthButton
+          onClick={() => {
+            auth.signOut();
+            router.push("/");
+          }}
+        >
+          로그아웃
+        </AuthButton>
+      )}
     </HeaderContainer>
   );
 }
